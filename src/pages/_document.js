e39233d996672d928a483340cef892d64d1c4229
goldnramen <adelsondabup@gmail.com>
2025-12-30 13:59:03 +0100

    Removed attempted import of headerDropDown from _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import {headerDropDown} from '@/context/BurgerContext'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className={`antialiased ${headerDropDown ? "overflow-hidden" : "overflow-auto"}`}>
+      <body className='antialiased'>
         <Main />
         <NextScript />
       </body>
